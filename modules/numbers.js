b983a568d1c6c9a884b0eefe4cb3cfb1a11cde29
Gustavo Abranches <gabranches@gmail.com>
2019-05-26 02:57:47 -0400

    currency fix

--- modules/numbers.js
+++ modules/numbers.js
@@ -1,53 +1,52 @@
-const self = {}
+const self = {};
 
-self.addCommas = x => {
-  if (isNaN(x)) return Error(x + ' is not a number.')
-  const xString = x.toString()
-  const xArray = xString.split('.')
-  let whole = xArray[0]
-  whole = whole.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-  if (xArray.length === 1) return whole
-  const decimal = xArray[1]
-  return whole + '.' + decimal
-},
+(self.addCommas = x => {
+  if (isNaN(x)) return Error(x + ' is not a number.');
+  const xString = x.toString();
+  const xArray = xString.split('.');
+  let whole = xArray[0];
+  whole = whole.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  if (xArray.length === 1) return whole;
+  const decimal = xArray[1];
+  return whole + '.' + decimal;
+}),
+  (self.format = (val, options = {}) => {
+    let num = Number.parseFloat(val);
+    let wholeNum = Math.round(val);
+    let precision = options.precision || 3;
 
-self.format = (val, options = {}) => {
-  let num = Number.parseFloat(val)
-  let wholeNum = Math.round(val)
-  let precision = options.precision || 3;
-
-  if (num === 0) {
-    if (options.dashedZero === true) return '-'
-    return '0'
-  }
-  if (num < .0001) {
-    return num.toExponential(1)
-  }
-  if (val > 0 && val < 100) {
-    if (options.currency) return num.toFixed(2);
-  }
-  if (val < 1000) {
-    return num.toPrecision(precision)
-  }
-  if (val < 1000000) {
-    return self.addCommas(wholeNum)
-  }
-  if (val < 1000000000) {
-    return (wholeNum / 1000000).toPrecision(precision) + 'M'
-  }
-  if (val < 1000000000000) {
-    return (wholeNum / 1000000000).toPrecision(precision) + 'B'
-  }
-  if (val < 1000000000000000) {
-    return (wholeNum / 1000000000000).toPrecision(precision) + 'T'
-  }
-}
+    if (num === 0) {
+      if (options.dashedZero === true) return '-';
+      return '0';
+    }
+    if (num <= 0.0001) {
+      return num.toExponential(1);
+    }
+    if (val >= 0.1 && val < 100) {
+      if (options.currency) return num.toFixed(2);
+    }
+    if (val < 1000) {
+      return num.toPrecision(precision);
+    }
+    if (val < 1000000) {
+      return self.addCommas(wholeNum);
+    }
+    if (val < 1000000000) {
+      return (wholeNum / 1000000).toPrecision(precision) + 'M';
+    }
+    if (val < 1000000000000) {
+      return (wholeNum / 1000000000).toPrecision(precision) + 'B';
+    }
+    if (val < 1000000000000000) {
+      return (wholeNum / 1000000000000).toPrecision(precision) + 'T';
+    }
+  });
 
 self.formatPercent = (val, decimals = 2) => {
   if (val === 0) {
-    return '-'
+    return '-';
   }
-  return parseFloat(val).toFixed(decimals) + '%'
-}
+  return parseFloat(val).toFixed(decimals) + '%';
+};
 
-module.exports = self
+module.exports = self;
--- tests/currencies.test.js
+++ tests/currencies.test.js
@@ -3,6 +3,10 @@ const currencies = require('../index').currencies;
 describe("currencies", () => {
   describe("format", () => {
     it("should apply the correct currency symbol to the number", () => {
+      expect(currencies.format(0.001234, "USD")).toBe("$0.00123");
+      expect(currencies.format(0.01234, "USD")).toBe("$0.0123");
+      expect(currencies.format(0.1234, "USD")).toBe("$0.12");
+      expect(currencies.format(1.234, "USD")).toBe("$1.23");
       expect(currencies.format(12.3, "USD")).toBe("$12.30");
       expect(currencies.format(12, "USD")).toBe("$12.00");
       expect(currencies.format(123, "USD")).toBe("$123");
